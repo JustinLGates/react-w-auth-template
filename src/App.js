1351b98ddf009ed55fb5b4c57db277f30c2e20c7
JustinLGates <papagates1@gmail.com>
2020-08-17 21:59:21 -0600

    fixed sets bearer on refresh

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "./App.css";
 import { Nav } from "./Components/Nav";
-import Testapirequest from "./Pages/Profile";
 import { Route } from "react-router-dom";
 import About from "./Pages/AboutPage";
 import Home from "./Pages/Home";
 import Profile from "./Pages/Profile";
+import { useAuth0 } from "@auth0/auth0-react";
+import { setBearer } from "./axios";
+
 function App() {
+  const { isAuthenticated, getAccessTokenSilently } = useAuth0();
+  (async () => setBearer("Bearer " + (await getAccessTokenSilently())))();
   return (
     <div className="">
       <Nav />
